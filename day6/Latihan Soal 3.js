0b0dcde1f5d6c59ff54daa921f1480d8d182900f
Kenichi <kenichi.jks@gmail.com>
2018-11-05 21:48:50 +0700

    Added file 3.

---
+++ day6/Latihan Soal 3.js
@@ -0,0 +1,16 @@
+var a = 1
+
+while(a < 100) {
+    a++;
+    var prime = true;
+    var b = 2
+    while(b < a) {
+        if(a % b === 0) {
+            prime = false
+        }
+        b++;
+    }
+    if(prime) {
+    console.log("Bilangan Prima = " + a)
+    }
+}
